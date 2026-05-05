21b9bd8a0ce496cf0059b9736590079cfa184904
vDevopsvinod <your-email@example.com>
2026-05-06 00:38:52 +0530

    Use SSM release_version for EKS 1.29 node group AMI

--- modules/eks-node-group/main.tf
+++ modules/eks-node-group/main.tf
@@ -1,8 +1,13 @@
+data "aws_ssm_parameter" "eks_nodegroup_release_version" {
+  name = "/aws/service/eks/optimized-ami/${var.cluster_version}/amazon-linux-2/recommended/release_version"
+}
+
 resource "aws_eks_node_group" "this" {
-  cluster_name    = var.cluster_name
-  node_group_name = var.node_group_name
-  node_role_arn   = var.node_role_arn
-  subnet_ids      = var.subnet_ids
+  cluster_name     = var.cluster_name
+  node_group_name  = var.node_group_name
+  node_role_arn    = var.node_role_arn
+  subnet_ids       = var.subnet_ids
+  release_version  = data.aws_ssm_parameter.eks_nodegroup_release_version.value
 
   scaling_config {
     desired_size = var.desired_size
