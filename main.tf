f8ca24c1c64836da57fa16b91a0c47b720f67b6c
vDevopsvinod <your-email@example.com>
2026-05-05 23:54:41 +0530

    Add DynamoDB state locking to prevent concurrent state conflicts

--- main.tf
+++ main.tf
@@ -8,9 +8,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "vinod-terraform-states"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "vinod-terraform-states"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
   }
 }
 
